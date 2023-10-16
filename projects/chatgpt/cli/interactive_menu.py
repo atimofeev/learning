ee8e56ac449b76e97e2575b848e8e48947a84185
Artem Timofeev <Legalaze94@gmail.com>
2023-10-16 11:16:11 +0400

    chatgpt-cli: linted and documented

--- projects/chatgpt/cli/interactive_menu.py
+++ projects/chatgpt/cli/interactive_menu.py
@@ -1,15 +1,28 @@
 """Generate interactive menu.
-Returns selected item."""
+
+Returns selected item.
+"""
 
 import curses
 
+
 def _menu(stdscr, items, message="Select a model:"):
+    """Display an interactive menu to select an item.
+
+    Args:
+        stdscr: Standard screen object provided by curses.
+        items: List of items to display in the menu.
+        message: Message displayed at the top of the menu.
+
+    Returns:
+        The selected item from the menu.
+    """
     curses.curs_set(0)
     current_row = 0
 
     while True:
         stdscr.clear()
-        stdscr.addstr(0, 0, message) 
+        stdscr.addstr(0, 0, message)
         for idx, item in enumerate(items):
             if idx == current_row:
                 stdscr.addstr(idx + 1, 0, f"> {item}")
@@ -22,14 +35,21 @@ def _menu(stdscr, items, message="Select a model:"):
             current_row -= 1
         elif key == curses.KEY_DOWN and current_row < len(items) - 1:
             current_row += 1
-        elif key == ord("\n"): 
+        elif key == ord("\n"):
             return items[current_row]
-        elif key in (4, ord('q')): # Handle EOF and 'q'
+        elif key in (4, ord('q')):  # Handle EOF and 'q'
             print()
             exit(0)
 
 
 def select(items):
-    """Accepts lists"""
+    """Invoke an interactive menu to select an item.
+
+    Args:
+        items: List of items to display in the menu.
+
+    Returns:
+        The selected item from the menu.
+    """
     selected_item = curses.wrapper(_menu, items)
     return selected_item
--- projects/chatgpt/cli/models_request.py
+++ projects/chatgpt/cli/models_request.py
@@ -1,16 +1,21 @@
-"""Requests available AI models from OpenAI"""
+"""Requests available AI models from OpenAI."""
 import requests
+
 
 def get_models():
     """Get model list from OpenAI.
-    Currently lists ones starting with 'gpt*'"""
+
+    Currently lists ones starting with 'gpt*'
+    """
     models = []
     with open("openai.key", encoding="utf-8") as f:
         openai_api_key = f.read().strip()
 
-    response = requests.get('https://api.openai.com/v1/models', \
-        headers={'Authorization': f'Bearer {openai_api_key}'}, \
-        timeout=10)
+    response = requests.get(
+        'https://api.openai.com/v1/models',
+        headers={'Authorization': f'Bearer {openai_api_key}'},
+        timeout=10
+    )
 
     json_data = response.json()
 
@@ -19,10 +24,14 @@ def get_models():
             models.append(item['id'])
     return models
 
+
 def _main():
-    """Main function for standalone use.
-    Prints models list."""
+    """Handle standalone use.
+
+    Prints models list.
+    """
     print(*get_models(), sep='\n', end='')
+
 
 if __name__ == "__main__":
     _main()
